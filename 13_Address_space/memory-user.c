48fc725c7746c808ea5e8cddc0946332f78f1e9e
Girija Prasanna <girijaprasanna1999@gmail.com>
2026-09-18 23:56:16 +0530

    memory_user and virtual address

---
+++ 13_Address_space/memory-user.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <memory_MB>\n", argv[0]);
+        return 1;
+    }
+
+    int mb = atoi(argv[1]);
+
+    // Allocate the requested amount of memory.
+    int *array = malloc(mb * 1024 * 1024);
+
+    if (array == NULL) {
+        perror("malloc");
+        return 1;
+    }
+
+    // Number of integers we can fit in the allocated memory.
+    long num_elements = (mb * 1024L * 1024L) / sizeof(int);
+
+    printf("Allocated %d MB\n", mb);
+
+    // Continuously touch every element.
+    while (1) {
+        for (long i = 0; i < num_elements; i++) {
+            array[i] += 1;
+        }
+    }
+
+    free(array);
+    return 0;
+}

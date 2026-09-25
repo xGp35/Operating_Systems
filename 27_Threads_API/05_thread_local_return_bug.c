c6bf3401929bb879b8815a084a44e8d7f792926b
xGp35 <girijaprasanna1999@gmail.com>
2026-09-26 02:45:39 +0530

    Add example demonstrating thread-local return values with pthreads

---
+++ 27_Threads_API/05_thread_local_return_bug.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "common_threads.h"
+
+typedef struct {int a; int b; } myarg_t;
+typedef struct {int x; int y; } myret_t;
+
+void *mythread(void *arg) {
+    myarg_t *args = (myarg_t *) arg;
+    printf("args %d %d\n", args->a, args->b);
+    myret_t oops;
+    oops.x = 1;
+    oops.y = 2;
+    return (void *) &oops;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t p;
+    myret_t *returned_values;
+    myarg_t args = { 10, 20 };
+    Pthread_create(&p, NULL, mythread, &args);
+    Pthread_join(p, (void **) &returned_values);
+    printf("returned %d %d\n", returned_values->x, returned_values->y);
+    free(returned_values);
+    return 0;
+}

360758c6980be4bdc13e14c4acaf2ac3829e7d67
xGp35 <girijaprasanna1999@gmail.com>
2026-09-26 12:43:04 +0530

    added all homework problems ,now onto solve them all for threads api chapter

--- 27_Threads_API/common_threads.h
+++ 27_Threads_API/common_threads.h
@@ -12,14 +12,18 @@
 #define Pthread_create(thread, attr, start_routine, arg) assert(pthread_create(thread, attr, start_routine, arg) == 0);
 #define Pthread_join(thread, value_ptr)                  assert(pthread_join(thread, value_ptr) == 0);
 
+#define Pthread_mutex_init(m, v)                         assert(pthread_mutex_init(m, v) == 0);
 #define Pthread_mutex_lock(m)                            assert(pthread_mutex_lock(m) == 0);
 #define Pthread_mutex_unlock(m)                          assert(pthread_mutex_unlock(m) == 0);
+
+#define Pthread_cond_init(cond, v)                       assert(pthread_cond_init(cond, v) == 0);
 #define Pthread_cond_signal(cond)                        assert(pthread_cond_signal(cond) == 0);
 #define Pthread_cond_wait(cond, mutex)                   assert(pthread_cond_wait(cond, mutex) == 0);
 
 #define Mutex_init(m)                                    assert(pthread_mutex_init(m, NULL) == 0);
 #define Mutex_lock(m)                                    assert(pthread_mutex_lock(m) == 0);
 #define Mutex_unlock(m)                                  assert(pthread_mutex_unlock(m) == 0);
+
 #define Cond_init(cond)                                  assert(pthread_cond_init(cond, NULL) == 0);
 #define Cond_signal(cond)                                assert(pthread_cond_signal(cond) == 0);
 #define Cond_wait(cond, mutex)                           assert(pthread_cond_wait(cond, mutex) == 0);
